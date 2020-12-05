406478c1b7c7912974a25b36a94f03c118033b52
albertdeveloper <rossalbertgonzales@gmail.com>
2020-12-05 18:37:41 +0800

    remove unused

--- app/Repositories/RoleRepositoryContract.php
+++ app/Repositories/RoleRepositoryContract.php
@@ -6,6 +6,5 @@ interface RoleRepositoryContract
 {
     public function process($request);
     public function allWithPermissions();
-    public function all();
     public function findById($id);
 }
