5c75f818d8d4a258df34529081998d08d0ddd7fc
albertdeveloper <rossalbertgonzales@gmail.com>
2020-12-07 02:25:37 +0800

    remove "save and new"

--- resources/js/Pages/Admin/Contacts/Create.vue
+++ resources/js/Pages/Admin/Contacts/Create.vue
@@ -9,11 +9,6 @@
                     Cancel
                 </Inertia-link>
 
-                <a @click.prevent="submitForm" v-if="$page.allowed_gates.includes('contacts_process')"
-                   class="cursor-pointer btn-primary mb-5 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-teal-700 hover:bg-teal-900 text-white font-normal py-2 px-4 mr-1 rounded">
-                    Save and New
-                </a>
-
                 <a @click.prevent="submitForm"  v-if="$page.allowed_gates.includes('contacts_process')"
                    class="cursor-pointer btn-primary mb-5 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-teal-700 hover:bg-teal-900 text-white font-normal py-2 px-4 mr-1 rounded">
                     Save
--- resources/js/Pages/Admin/Leads/Create.vue
+++ resources/js/Pages/Admin/Leads/Create.vue
@@ -11,11 +11,6 @@
 
                 <a @click.prevent="submitForm" v-if="$page.allowed_gates.includes('leads_process')"
                    class="cursor-pointer btn-primary mb-5 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-teal-700 hover:bg-teal-900 text-white font-normal py-2 px-4 mr-1 rounded">
-                    Save and New
-                </a>
-
-                <a @click.prevent="submitForm" v-if="$page.allowed_gates.includes('leads_process')"
-                   class="cursor-pointer btn-primary mb-5 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-teal-700 hover:bg-teal-900 text-white font-normal py-2 px-4 mr-1 rounded">
                     Save
                 </a>
             </div>
